f10c51ea2538780bc2ab4374af5dede50c86ff85
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2024-07-25 16:11:53 -0600

    feat: Improve testing script

--- test.py
+++ test.py
@@ -1,9 +1,35 @@
 from transformers import pipeline
 
+import random
+
 fill_mask = pipeline(
     'fill-mask',
     model = './models/reginald',
     tokenizer = './models/reginald-tokens'
 )
 
-print(fill_mask(input() + "<mask>"))
+def next_token(past, k = 20):
+    tokens = fill_mask(past + '<mask>', top_k = k)
+
+    total = 0.0
+
+    for guess in tokens:
+        total += guess['score']
+
+    choice = random.uniform(0.0, total)
+
+    for guess in tokens:
+        choice -= guess['score']
+
+        if choice <= 0:
+            return guess
+        
+    return tokens[-1]
+
+
+current = ''
+
+while True:
+    current = current + next_token(current)['token_str']
+    print(current)
+    input()
