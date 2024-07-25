658a38428e6a9fba16208f9fb80cc08424e26149
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2024-07-25 12:50:41 -0600

    feat: Make function for getting city names

---
+++ get_names.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def get_city_names():
+    cities_df = pd.read_csv('./data/cities.csv', sep = ';', encoding = 'utf8')
+
+    datasets = { }
+    big_data = [ ]
+
+    print('Extracting city names')
+
+    for _, row in cities_df.iterrows():
+        country = row['Country name EN']
+        city = row['Name']
+
+        if not isinstance(city, str) or not isinstance(country, str):
+            continue
+
+        big_data.append(city)
+
+        if country in datasets:
+            datasets[country].append(city)
+        else:
+            datasets[country] = [ city ]
+    
+    return datasets, big_data
--- tokenizer.py
+++ tokenizer.py
@@ -1,26 +1,6 @@
-# Extract names
-import pandas as pd
+from get_names import get_city_names
 
-cities_df = pd.read_csv('./data/cities.csv', sep = ';', encoding = 'utf8')
-
-datasets = { }
-big_data = [ ]
-
-print('Extracting city names')
-
-for _, row in cities_df.iterrows():
-    country = row['Country name EN']
-    city = row['Name']
-
-    if not isinstance(city, str) or not isinstance(country, str):
-        continue
-
-    big_data.append(city)
-
-    if country in datasets:
-        datasets[country].append(city)
-    else:
-        datasets[country] = [ city ]
+_, names = get_city_names()
 
 # Tokenize names
 from tokenizers import ByteLevelBPETokenizer
@@ -28,7 +8,7 @@ from tokenizers import ByteLevelBPETokenizer
 tokenizer = ByteLevelBPETokenizer()
 
 tokenizer.train_from_iterator(
-    big_data,
+    names,
     vocab_size = 20_000,
     min_frequency = 2,
     special_tokens = [
@@ -40,4 +20,4 @@ tokenizer.train_from_iterator(
     ]
 )
 
-tokenizer.save_model('./tokenton', 'tokenton')
+tokenizer.save_model('./models/tokenton', 'tokenton')
