5585687ef4b5716ad76c7cc189ac27e9ce03dbd9
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2024-07-26 08:22:37 -0600

    feat: Simpler test using huggingface pipeline

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-from transformers import AutoTokenizer
+from transformers import pipeline
 
-prompt = input('Start of name: ')
+generator = pipeline(model = './models/reginald', tokenizer = './models/reginald-tokens', task = 'text-generation', do_sample = True, max_new_tokens = 10)
 
-tokenizer = AutoTokenizer.from_pretrained('./models/reginald-tokens')
-inputs = tokenizer(prompt, return_tensors = 'pt').input_ids
-
-from transformers import AutoModelForCausalLM
-
-model = AutoModelForCausalLM.from_pretrained('./models/reginald')
-outputs = model.generate(inputs, max_new_tokens = 10, do_sample = True, top_k = 50, top_p = 0.5, num_beams = 3, eos_token_id = model.config.eos_token_id)
-
-print(tokenizer.batch_decode(outputs, skip_special_tokens = True))
+print('\n'.join([ '* ' + generator(letter)[0]['generated_text'] for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' ]))
